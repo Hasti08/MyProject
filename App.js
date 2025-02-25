95b98ca92ae117ddda34595686a11c88e9dba928
Hasti <hastisatani12@gmail.com>
2025-02-25 12:00:57 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import Navigator from "./Navigation/Navigator";
+import FlashMessage from "react-native-flash-message";
+import 'react-native-gesture-handler';
+import { MMKV } from 'react-native-mmkv'
+
+
+export const storage = new MMKV();
+
+function App () {
+     
+    return (<>
+                <Navigator/>
+                <FlashMessage position="top" duration={3000} />
+            </>);
+}
+
+export default App;
